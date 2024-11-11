bc390fd0207cad3944cb5cba482315664ffb17e3
Cameron Emery <75042648+mESmaC@users.noreply.github.com>
2024-11-10 19:54:54 -0500

    changes to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,21 @@ import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'node:path';
-import { netlifyPlugin } from '@netlify/remix-adapter/plugin';
 
 export default defineConfig({
+    base: '/', 
     plugins: [
         remix({
             ignoredRouteFiles: ['**/*.module.scss'],
         }),
         tsconfigPaths(),
-        netlifyPlugin(),
     ],
     resolve: {
         alias: {
             '@styles': path.resolve(__dirname, './src/styles/'),
         },
     },
+    build: {
+        outDir: 'dist'
+    }
 });
