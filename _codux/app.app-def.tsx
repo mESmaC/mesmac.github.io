70c6f51a6ce1b2f2219052ba0f41fd7885514c4b
Cameron Emery <75042648+mESmaC@users.noreply.github.com>
2024-11-10 20:20:11 -0500

    different approach

--- _codux/app.app-def.tsx
+++
@@ -1,6 +0,0 @@
-import defineRemixApp from '@wixc3/define-remix-app';
-
-export default defineRemixApp({
-    appPath: '../app',
-    routingPattern: 'folder(route)',
-});
--- _codux/boards/content/content.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Content } from '../../../src/components/content/content';
-
-export default createBoard({
-    name: 'Content',
-    Board: () => <Content />,
-});
